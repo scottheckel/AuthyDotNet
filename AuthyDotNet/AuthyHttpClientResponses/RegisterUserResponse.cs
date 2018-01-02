646c6ba1224d9f6bc52d1a5ea23780da909300f4
Scott Heckel <scott.heckel@gmail.com>
2018-01-02 10:37:28 -0600

    Fixing response issues

--- AuthyDotNet/AuthyHttpClientResponses/RegisterUserResponse.cs
+++ AuthyDotNet/AuthyHttpClientResponses/RegisterUserResponse.cs
@@ -1,6 +1,6 @@
 ﻿namespace AuthyDotNet.AuthyHttpClientResponses
 {
-    public class RegisterUserResponse
+    public class RegisterUserResponse : AuthyResponse
     {
         public UserResponse User { get; set; }
         public int UserId => User.Id;
